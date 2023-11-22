87439c6e6a30fd299c5ddc00cdddebfbfc0e06fe
EMMANUEL MULWA MUTUA <94626975+emmanuel-mutua@users.noreply.github.com>
2023-11-22 07:03:00 +0300

    Revert "sign-up"

--- app/src/main/java/com/example/screendesign/Authentication/SIGNUp.java
+++ app/src/main/java/com/example/screendesign/Authentication/SIGNUp.java
@@ -1,17 +1,11 @@
 package com.example.screendesign.Authentication;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.screendesign.R;
@@ -19,9 +13,6 @@ import com.example.screendesign.R;
 public class SIGNUp extends AppCompatActivity {
     TextView LoginTextview;
     Button SignUpButton;
-    Uri selectedImageUri;
-    ImageView imageView;
-    Button chooseProfilePictureButton;
     EditText EmailEditText, PasswordEditText, ConfirmPasswordEditText,
             UsernameEditText,PhoneEditText,GenderEditText, shortBioEditText,skillsEditText;
 
@@ -29,8 +20,6 @@ public class SIGNUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        imageView = findViewById(R.id.profileImageView);
-        chooseProfilePictureButton = findViewById(R.id.chooseProfilePictureButton);
         LoginTextview = findViewById(R.id.login_textView);
         SignUpButton = findViewById(R.id.SignUp_button);
         EmailEditText = findViewById(R.id.Email_editText);
@@ -44,23 +33,5 @@ public class SIGNUp extends AppCompatActivity {
         LoginTextview.setOnClickListener(v -> {
             startActivity(new Intent(SIGNUp.this, Login.class));
         });
-        chooseProfilePictureButton.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            chooseProfilePictureLauncher.launch(intent);
-        });
     }
-    private final ActivityResultLauncher<Intent> chooseProfilePictureLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    // Handle the result, e.g., get the selected image URI
-                    Intent data = result.getData();
-                    if (data != null && data.getData() != null) {
-                        selectedImageUri = data.getData();
-                        imageView.setImageURI(selectedImageUri);
-                    }
-                }
-            }
-    );
-
 }
